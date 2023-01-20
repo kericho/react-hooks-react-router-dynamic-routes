58883be485b67e1cf4ae81d7396c0daefba94377
¨kericho¨ <lvisrono90@gmail.com>
2023-01-20 17:14:53 +0300

    completed

---
+++ src/components/MovieShow.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function MovieShow() {
+    return(
+        <div>
+            <h3>Movies Show Component!</h3>
+        </div>
+    )
+}
+
+export default MovieShow
