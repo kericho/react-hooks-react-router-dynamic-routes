692e91dc5c774e7ae1fa00cac6964f34e5deb279
¨kericho¨ <lvisrono90@gmail.com>
2023-01-31 09:23:40 +0300

    redo components

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,15 +1,14 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 function MoviesList({ movies }) {
-    const renderMovies = Object.keys(movies).map((movieID) => (
-      <li key={movieID}>
-        <Link to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
-      </li>
-    ));
-  
-    return <ul>{renderMovies}</ul>
-  }
-  
-  export default MoviesList;
-  
+	const renderMovies = Object.keys(movies).map((movieID) => (
+		<li key={movieID}>
+			<Link to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
+		</li>
+	));
+
+	return <ul>{renderMovies}</ul>;
+}
+
+export default MoviesList;
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -4,22 +4,20 @@ import MoviesList from "./MoviesList";
 import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
-  const match = useRouteMatch();
-  console.log(match)
+	const match = useRouteMatch();
+	console.log(match);
 
-  return (
-    <div>
-      <MoviesList movies={movies} />
+	return (
+		<div>
+			<MoviesList movies={movies} />
+			<Route exact path={match.url}>
+				<h3>Choose a movie from the list above</h3>
+			</Route>
 
-      <Route exact path={match.url}>
-        <h3>Choose a movie from the list above</h3>
-      </Route>
-      
-      <Route path={`${match.url}/:movieId`}>
-        <MovieShow movies={movies} />
-      </Route>
-    </div>
-  );
+			<Route path={`${match.url}/:movieId`}>
+				<MovieShow movies={movies} />
+			</Route>
+		</div>
+	);
 }
-
 export default MoviesPage;
